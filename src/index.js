0f66df4d6ce0a84a5f923f4673d615329c5e9ba3
Anna-Sh <a.shmidova@yandex.ru>
2018-09-30 10:42:11 +0300

    zero

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+
+    let zeros = 0;
+
+    for (let mult = 5; mult <= number; mult *= 5) {
+        zeros += Math.floor(number / mult);
+    }
+
+    return zeros;
+};
+
+
